038101488a115150d5bd72e135b823e16c18d887
Ruchir Ranjan <you@example.com>
2021-07-10 14:20:24 +0530

    Day34

---
+++ 50Day50Projects/Day34/script.js
@@ -0,0 +1,4 @@
+const nums = document.querySelectorAll('.nums span')
+const counter = document.querySelector('.counter')
+const counter = document.querySelector('.counter')
+const counter = document.querySelector('.counter')
