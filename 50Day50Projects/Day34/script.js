8c2994d630fec8b79d80121491753a3c947f901d
Ruchir Ranjan <you@example.com>
2021-07-10 15:04:01 +0530

    Day34

--- 50Day50Projects/Day34/script.js
+++ 50Day50Projects/Day34/script.js
@@ -1,4 +1,20 @@
-const nums = document.querySelectorAll('.nums span')
-const counter = document.querySelector('.counter')
-const counter = document.querySelector('.counter')
-const counter = document.querySelector('.counter')
+const nums = document.querySelectorAll('.nums span');
+const counter = document.querySelector('.counter');
+const final = document.querySelector('.final');
+const replay = document.querySelector('#replay');
+
+runAnimation();
+
+function runAnimation() {
+  nums.forEach((num, idx) => {
+    const nextToLast = num.length - 1;
+
+    num.addEventListener('animationed', (e) => {
+      if (e.animationName === 'goIn' && idx !== nextToLast) {
+        num.classList.remove('in');
+        num.classList.add('out');
+      } else if (e.animationName === 'goOut' && num.nextElementSibling) {
+      }
+    });
+  });
+}
