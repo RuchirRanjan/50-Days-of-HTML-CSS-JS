dc4c14c32311c7a18ac9f4e54e9847066f95580c
Ruchir Ranjan <you@example.com>
2021-08-04 19:40:28 +0530

    Project

--- 50Day50Projects/Day35/script.js
+++ 50Day50Projects/Day35/script.js
@@ -1,8 +1,11 @@
 const imgs = document.getElementById('imgs');
 const leftBtn = document.getElementById('left');
 const rightBtn = document.getElementById('right');
+
 const img = document.querySelectorAll('#imgs img');
-let idx = 0;
+
+let idx = 1;
+
 let interval = setInterval(run, 2000);
 
 function run() {
@@ -11,11 +14,47 @@ function run() {
 }
 
 function changeImage() {
-  if (idx > img.length) {
-    idx = 0;
-  } else if (idx < 0) {
+  if (idx > img.length - 1) {
     idx = img.length - 1;
-  }
 
-  imgs.style.transform = `translateX(${-idx * 500}px)`;
+    imgs.style.transition = 'all, linear, 1s';
+    imgs.style.transform = `translateX(${-idx * 500}px)`;
+
+    setTimeout(function () {
+      idx = 1;
+      imgs.style.transition = '';
+      imgs.style.transform = `translateX(${-500}px)`;
+    }, 0);
+  } else if (idx < 0) {
+    idx = 0;
+
+    imgs.style.transition = 'all, linear, 1s';
+    imgs.style.transform = `translateX(${-idx * 500}px)`;
+
+    setTimeout(function () {
+      idx = img.length - 2;
+      imgs.style.transition = '';
+      imgs.style.transform = `translateX(${-idx * 500}px)`;
+    }, 0);
+  } else {
+    imgs.style.transition = 'all, linear, 1s';
+    imgs.style.transform = `translateX(${-idx * 500}px)`;
+  }
 }
+
+function resetInterval() {
+  clearInterval(interval);
+  interval = setInterval(run, 2000);
+}
+
+rightBtn.addEventListener('click', () => {
+  idx++;
+  changeImage();
+  resetInterval();
+});
+
+leftBtn.addEventListener('click', () => {
+  idx--;
+  changeImage();
+  resetInterval();
+});
