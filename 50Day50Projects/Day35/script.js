bec237953ff5e56eb268993f961f279918d1cda6
Ruchir Ranjan <you@example.com>
2021-08-04 19:37:15 +0530

    Project

--- 50Day50Projects/Day35/script.js
+++ 50Day50Projects/Day35/script.js
@@ -1,6 +1,21 @@
-const imgs = document.getElementById("imgs");
-const leftBtn = document.getElementById("left");
-const rightBtn = document.getElementById("right");
-const img - document.querySelectorAll('#imgs img')
-let idx = 0
-let interval = setInterval(run, 2000)
+const imgs = document.getElementById('imgs');
+const leftBtn = document.getElementById('left');
+const rightBtn = document.getElementById('right');
+const img = document.querySelectorAll('#imgs img');
+let idx = 0;
+let interval = setInterval(run, 2000);
+
+function run() {
+  idx++;
+  changeImage();
+}
+
+function changeImage() {
+  if (idx > img.length) {
+    idx = 0;
+  } else if (idx < 0) {
+    idx = img.length - 1;
+  }
+
+  imgs.style.transform = `translateX(${-idx * 500}px)`;
+}
