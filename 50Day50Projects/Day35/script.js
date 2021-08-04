941e46a26bba6c42a593a5be2a1f2a92e2e7f463
Ruchir Ranjan <you@example.com>
2021-08-04 19:29:43 +0530

    Project

---
+++ 50Day50Projects/Day35/script.js
@@ -0,0 +1,6 @@
+const imgs = document.getElementById("imgs");
+const leftBtn = document.getElementById("left");
+const rightBtn = document.getElementById("right");
+const img - document.querySelectorAll('#imgs img')
+let idx = 0
+let interval = setInterval(run, 2000)
