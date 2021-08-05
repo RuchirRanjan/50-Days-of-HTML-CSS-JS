1d25ce453ad7dd6a73092ac19c2810b647bf8cb0
Ruchir Ranjan <you@example.com>
2021-08-05 21:20:03 +0530

    Project

--- 50Day50Projects/Day36/script.js
+++ 50Day50Projects/Day36/script.js
@@ -1,2 +1,3 @@
 const container = document.getElementById('container');
-const colors = ['#00ffff', '#6495ed', '#6a5acd', '#00ff7f', '#87ceeb',Project'#e74c3c', '#8e44ad', '#e67e22']
+const colors = ['#00ffff', '#6495ed', '#6a5acd', '#00ff7f', '#87ceeb','#e74c3c', '#8e44ad', '#e67e22']
+const SQUARES = 500
