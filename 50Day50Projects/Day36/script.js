0bab9714a4dd3f4c8c142b6ec86011e08f80be59
Ruchir Ranjan <you@example.com>
2021-08-05 21:52:42 +0530

    Project

--- 50Day50Projects/Day36/script.js
+++ 50Day50Projects/Day36/script.js
@@ -27,7 +27,10 @@ function setColor(element) {
   element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
 }
 
-function removeColor(element) {}
+function removeColor(element) {
+    element.style.background = '#1d1d1d'
+    element.style.boxShadow = '0 0 2px #000'
+}
 
 
 function getRandomColor() {
