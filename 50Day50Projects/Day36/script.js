3476a9598bbe0947d4ddec9fddb05c0d67733223
Ruchir Ranjan <you@example.com>
2021-08-05 21:22:55 +0530

    Project

--- 50Day50Projects/Day36/script.js
+++ 50Day50Projects/Day36/script.js
@@ -1,3 +1,19 @@
 const container = document.getElementById('container');
-const colors = ['#00ffff', '#6495ed', '#6a5acd', '#00ff7f', '#87ceeb','#e74c3c', '#8e44ad', '#e67e22']
-const SQUARES = 500
+const colors = [
+  '#00ffff',
+  '#6495ed',
+  '#6a5acd',
+  '#00ff7f',
+  '#87ceeb',
+  '#e74c3c',
+  '#8e44ad',
+  '#e67e22',
+];
+const SQUARES = 500;
+
+for (let i = 0; i < SQUARES; i++) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+
+  container.appendChild(square);
+}
